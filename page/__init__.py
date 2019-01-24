d4f508bd83914742b6985263be135fa41305cf08
jiashengdan <18348708288>
2019-01-24 16:43:08 +0800

    aaaaaa

---
+++ page/__init__.py
@@ -0,0 +1,6 @@
+#各个元素的信息
+from selenium.webdriver.common.by import By
+
+click_more = By.XPATH,"//*[contains(@text,'更多')]"
+click_network = By.XPATH,"//*[contains(@text,'移动网络')]"
+click_network_type = By.XPATH,"//*[contains(@text,'首选网络类型')]"
